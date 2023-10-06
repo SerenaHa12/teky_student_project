f66d4dd8808881ecf5927cd9319507edde600b74
SerenaHa12 <serenavander.ha@gmail.com>
2023-10-06 18:59:53 +0700

    Update src

---
+++ asset/main.js
@@ -0,0 +1,12 @@
+function github() {
+  open(`https://github.com`);
+}
+function youtube() {
+  open(`https://www.youtube.com`);
+}
+function server() {
+  open(`https://discord.gg`);
+}
+function tiktok() {
+  open(`https://www.tiktok.com`);
+}
